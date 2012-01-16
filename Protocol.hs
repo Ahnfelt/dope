733accd88058d6f5f2db2e2aae13ee957b14397b
Michael Werk <werk@HP.(none)>
2012-01-16 17:47:23 +0100

    Started on a console client

---
+++ Protocol.hs
@@ -0,0 +1,27 @@
+module Protocol where
+
+import Dope
+import State
+
+data Request
+    = UsePlayer String
+    | NewPlayer String
+    | Act Option
+    | Quit
+    deriving (Show, Read, Eq)
+    
+data Response
+    = OK Player [Option]
+    | Error Error
+    | Bye
+    deriving (Show, Read, Eq)
+
+data Error
+    = InvalidRequest
+    | IlligalAct String Player [Option]
+    | PlayerDoesNotExist
+    | NotLoggedIn
+    | PlayerAlreadyExist
+    deriving (Show, Read, Eq)
+    
+data Client = Client (Request -> IO (Either Response (Response, Client)))
