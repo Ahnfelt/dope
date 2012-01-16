be062b81874a8adc6e32c9ae0c58366a81b2e477
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2012-01-16 21:52:44 +0100

    Reduced view types for Player and Site

--- Serialize.hs
+++ Serialize.hs
@@ -1,8 +1,53 @@
+{-# LANGUAGE TemplateHaskell, TypeOperators, KindSignatures #-}
 module Serialize where
 
 import State
 import Text.JSON
 import Control.Monad
+import Control.Applicative.Error (maybeRead)
+import Data.Label
+
+data PlayerAppearance = PlayerAppearance {
+    _playerAppearanceName :: String
+    }
+$(mkLabels [''PlayerAppearance])
+
+toPlayerAppearance :: Player -> PlayerAppearance
+toPlayerAppearance player = PlayerAppearance (get playerName player)
+
+instance JSON PlayerAppearance where
+    readJSON json = do
+        object <- readJSON json
+        Just name <- valFromObj "name" object
+        return (PlayerAppearance name)
+    showJSON playerAppearance = 
+        showJSON (toJSObject [
+            ("name", showJSON $ get playerAppearanceName playerAppearance)
+        ])
+
+data SiteExterior = SiteExterior {
+    _siteExteriorName :: String,
+    _siteExteriorType :: SiteType,
+    _siteExteriorPosition :: Position
+    }
+$(mkLabels [''SiteExterior])
+
+toSiteExterior :: Site -> SiteExterior
+toSiteExterior site = SiteExterior (get siteName site) (get siteType site) (get sitePosition site)
+
+instance JSON SiteExterior where
+    readJSON json = do
+        object <- readJSON json
+        Just name <- valFromObj "name" object
+        Just siteType <- valFromObj "type" object
+        Just position <- valFromObj "position" object
+        return (SiteExterior name siteType position)
+    showJSON siteExterior = 
+        showJSON (toJSObject [
+            ("name", showJSON $ get siteExteriorName siteExterior),
+            ("type", showJSON $ get siteExteriorType siteExterior),
+            ("position", showJSON $ get siteExteriorPosition siteExterior)
+        ])
 
 instance JSON Place where
     readJSON json = msum [
@@ -24,5 +69,32 @@ instance JSON Position where
         Just y <- valFromObj "y" object
         return (Position x y)
     showJSON (Position x y) =
-        showJSON [("x", x), ("y", y)]
+        showJSON $ toJSObject [
+            ("x", x), ("y", y)
+            ]
+
+instance JSON Situation where
+    readJSON json = msum [
+            do 
+                ["Idle"] <- readJSON json
+                return Idle,
+            do 
+                ["Busted"] <- readJSON json
+                return Busted,
+            do 
+                ("Trading", playerName) <- readJSON json
+                return (Trading playerName)
+        ]
+    showJSON situation = case situation of
+        Idle -> showJSON ["Idle"]
+        Busted -> showJSON ["Busted"]
+        Trading playerName -> showJSON ("Trading", playerName)
+
+instance JSON SiteType where
+    readJSON json = do
+        [tag] <- readJSON json
+        case maybeRead tag of
+            Just value -> return value
+            Nothing -> fail "Failed to read SiteType"
+    showJSON siteType = showJSON [show siteType]
 
